052b47210331bdf330d18bf729b150aa9dd91abb
hyunie47 <cfhyunie47@gmail.com>
2018-11-22 15:25:24 +0900

    second

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <strlen.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
+int main(int argc, char *argv[]) {
 	char src[100] = "Programming course";
-	char dst[100];
 	int i=0;
 	
 	while (src[i] != '\0')
 	{
-		dst[i] = src[i];
+	//	dst[i] = src[i];
 		i++;
 	}
-	dst[i]='\0';
+	//dst[i] = '\0';
 	
-	printf("dst : %s\n", dst);
+	//printf("dst : %s\n", dst);
+	printf("%s (%i)\n", src, i);
+	printf("%i", strlen(src));
 	
 	return 0;
 }
